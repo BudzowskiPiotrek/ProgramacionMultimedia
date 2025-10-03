6ce0934d13c2c91016592c1aaf9c1fab4c57ddef
Piotrek Budzowski <piobudzows@gmail.com>
2025-10-03 12:23:52 +0200

    .

--- Android/app/src/main/java/com/example/proyectouno/MainActivity.java
+++ Android/app/src/main/java/com/example/proyectouno/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.proyectouno;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
+    private EditText et1, et2;
+    private TextView tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.ejercicio3);
+        setContentView(R.layout.activity_main);
 
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+        tv3 = (TextView) findViewById(R.id.tv3);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    //Este método se ejecutará cuando se presione el botón
+    public void sumar(View view) {
+        int nro1 = Integer.parseInt(et1.getText().toString());
+        int nro2 = Integer.parseInt(et2.getText().toString());
+        int suma = nro1 + nro2;
+        String resu = String.valueOf(suma);
+        tv3.setText(resu);
+    }
 
 }
-
