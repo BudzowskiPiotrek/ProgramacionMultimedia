f00df588c59643141356f5859d13d78f703a731d
Piotrek Budzowski <piobudzows@gmail.com>
2025-10-22 14:36:50 +0200

    .

--- RadioButton/app/src/main/java/com/example/radiobutton/MainActivity.java
+++ RadioButton/app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -2,16 +2,18 @@ package com.example.radiobutton;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
 
-    public RadioButton rbTrasporte, rbHotel, rbComida, rbOcio;
-    public EditText etTrasporte, etHotel, etComida, etOcio;
+public class MainActivity extends AppCompatActivity {
+    public RadioGroup rgTransporte, rgHotel, rgComida, rgOcio;
+    public EditText etTrasporte, etHotel, etComida, etOcio, suma;
     private String[] opcionesTransporte, opcionesHotel, opcionesComida, opcionesOcio;
 
     @Override
@@ -19,22 +21,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        rbTrasporte = (RadioButton) findViewById(R.id.rbTransporte);
-        rbHotel = (RadioButton) findViewById(R.id.rbHotel);
-        rbComida = (RadioButton) findViewById(R.id.rbComida);
-        rbOcio = (RadioButton) findViewById(R.id.rbOcio);
+        rgTransporte = (RadioGroup) findViewById(R.id.rgTransporte);
+        rgHotel = (RadioGroup) findViewById(R.id.rgHotel);
+        rgComida = (RadioGroup) findViewById(R.id.rgComida);
+        rgOcio = (RadioGroup) findViewById(R.id.rgOcio);
+
         etTrasporte = (EditText) findViewById(R.id.etTrasporte);
         etHotel = (EditText) findViewById(R.id.etHotel);
         etComida = (EditText) findViewById(R.id.etComida);
         etOcio = (EditText) findViewById(R.id.etOcio);
+        
+        suma = (EditText) findViewById(R.id.suma);
 
         cargarArrays();
 
         rellenarContenedor();
 
-        setListeners();
+        //setListeners();
     }
-
 
     private void cargarArrays() {
         Resources res = getResources();
@@ -43,10 +47,19 @@ public class MainActivity extends AppCompatActivity {
         opcionesComida = res.getStringArray(R.array.Comida);
         opcionesOcio = res.getStringArray(R.array.Ocio);
     }
-
     private void rellenarContenedor() {
+        rellenarSeccion(opcionesTransporte, rgTransporte);
+        rellenarSeccion(opcionesHotel, rgHotel);
+        rellenarSeccion(opcionesComida, rgComida);
+        rellenarSeccion(opcionesOcio, rgOcio);
+    }
+    private void rellenarSeccion(String[] listaOpciones, RadioGroup rg) {
+        for (String opcion : listaOpciones) {
+            RadioButton rgBtn = new RadioButton(this);
+            rgBtn.setText(opcion);
+            rgBtn.setId(View.generateViewId());
+            rg.addView(rgBtn);
+        }
     }
 
-    private void setListeners() {
-    }
 }
